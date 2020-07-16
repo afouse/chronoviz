b5f001a645a943e87553c42bfe1bd1338253b0ba
Johannes Maas <johannes.maas1@rwth-aachen.de>
2020-07-16 18:00:23 +0200

    Make AverageColor plugin output series data

--- Plugin Examples/AverageColor.py
+++ Plugin Examples/AverageColor.py
@@ -1,9 +1,14 @@
+import math
+import struct
+import sys
+
 import objc
 
 from Foundation import *
 from AppKit import *
 from CoreMedia import *
 from Quartz import *
+from AVFoundation import *
 
 from Cocoa import *
 
@@ -11,45 +16,6 @@ PluginClass = objc.lookUpClass("AnnotationDataAnalysisPlugin")
 AnnotationCategoryFilter = objc.lookUpClass("AnnotationCategoryFilter")
 VideoFrameLoader = objc.lookUpClass("VideoFrameLoader")
 
-class AverageColor(PluginClass):
-    def setup(self):
-         # Sets the name of the plugin in the menu
-        self.setDisplayName_("Average color")
-    
-    def performAnalysis(self):
-        app = NSApplication.sharedApplication()
-
-        win = NSWindow.alloc()
-        win.retain()
-        frame = ((200.0, 300.0), (520.0, 560.0))
-        win.initWithContentRect_styleMask_backing_defer_(frame, 15, 2, 0)
-        win.setTitle_("Average color")
-        win.setLevel_(3)  # floating window
-
-        hel = NSButton.alloc().initWithFrame_(((10.0, 10.0), (80.0, 20.0)))
-        win.contentView().addSubview_(hel)
-        hel.setBezelStyle_(4)
-        hel.setTitle_("Calculate")
-        hel.setTarget_(self)
-        hel.setAction_("calculate")
-        
-        frameTime = CMTimeMakeWithSeconds(1, 600)
-        frameRef = VideoFrameLoader.generateImageAt_for_error_(frameTime, self.currentDocument().movie(), objc.NULL)
-        image = NSImage.alloc().initWithCGImage_size_(frameRef, NSZeroSize)
-        self.imageView = ImageSelection.alloc().initWithFrame_(((10, 50), (500, 500)))
-        self.imageView.setImage_(image)
-        self.imageView.setImageAlignment_(NSImageAlignBottom)
-        win.contentView().addSubview_(self.imageView)
-
-        win.makeKeyAndOrderFront_(self)
-        
-    def calculate(self):
-        selection = self.imageView.getSelectionInImagePixels()
-        print(NSStringFromRect(selection))
-
-    def annotationsForCategory_(self, categoryName):
-        category = self.categoryForName_(categoryName)
-        return self.currentDocument().annotationsForCategory_(category)
 
 # If the class already exists, catch the exception
 # ChronoViz prevents the "main" class in this file from such clashes, but not other classes.
@@ -129,5 +95,87 @@ try:
 except objc.error as e:
     if str(e).endswith("is overriding existing Objective-C class"):
         print(f"Did not reload class: {e}")
+        # Load the existing class so that it is available in the following.
+        ImageSelection = objc.lookUpClass("ImageSelection")
     else:
         raise e
+
+class AverageColor(PluginClass):
+    def setup(self):
+         # Sets the name of the plugin in the menu
+        self.setDisplayName_("Average color")
+    
+    def performAnalysis(self):
+        app = NSApplication.sharedApplication()
+
+        self.win = NSWindow.alloc()
+        self.win.retain()
+        frame = ((200.0, 300.0), (520.0, 560.0))
+        self.win.initWithContentRect_styleMask_backing_defer_(frame, 15, 2, 0)
+        self.win.setTitle_("Average color")
+        self.win.setLevel_(3)  # floating window
+
+        hel = NSButton.alloc().initWithFrame_(((10.0, 10.0), (80.0, 20.0)))
+        self.win.contentView().addSubview_(hel)
+        hel.setBezelStyle_(4)
+        hel.setTitle_("Calculate")
+        hel.setTarget_(self)
+        hel.setAction_("calculate")
+        
+        frameTime = CMTimeMakeWithSeconds(1, 600)
+        frameRef = VideoFrameLoader.generateImageAt_for_error_(frameTime, self.currentDocument().movie(), objc.NULL)
+        image = NSImage.alloc().initWithCGImage_size_(frameRef, NSZeroSize)
+        self.imageView = ImageSelection.alloc().initWithFrame_(((10, 50), (500, 500)))
+        self.imageView.setImage_(image)
+        self.imageView.setImageAlignment_(NSImageAlignBottom)
+        self.win.contentView().addSubview_(self.imageView)
+
+        self.win.makeKeyAndOrderFront_(self)
+        
+    def calculate(self):
+        selection = self.imageView.getSelectionInImagePixels()
+        
+        frameRate = self.getFrameRate()
+        durationInSeconds = CMTimeGetSeconds(self.currentDocument().movie().currentItem().duration())
+        end = min(math.floor(frameRate * durationInSeconds), 500)
+        
+        series = TimeSeriesData.alloc().init()
+        series.setName_("Average color data")
+        dataSource = PluginDataSource.alloc().initWithPath_(None)
+        dataSource.setName_("Average color")
+        dataSource.addDataSet_(series)
+        
+        for frameCount in range(0, end):
+            frameTime = CMTimeMake(frameCount, frameRate)
+            frameRef = VideoFrameLoader.generateImageAt_for_error_(frameTime, self.currentDocument().movie(), objc.NULL)
+            pixels = self.getPixels_forSelection_(frameRef, selection.origin)
+            pixel, = struct.unpack("B", pixels["r"])
+            series.addValue_atTime_(pixel, frameTime)
+            if frameCount % frameRate == 0:
+                self.log_(str(CMTimeGetSeconds(frameTime)) + ": " + str(pixel))
+        
+        AppController.currentApp().viewManager().showData_(series)
+        self.win.close()
+        
+    def getFrameRate(self):
+        asset = self.currentDocument().movie().currentItem().asset()
+        return asset.tracksWithMediaType_(AVMediaTypeVideo).objectAtIndex_(0).nominalFrameRate()
+        
+    def getPixels_forSelection_(self, imageRef, selectionPoint):
+        rawData = CGDataProviderCopyData(CGImageGetDataProvider(imageRef))
+        buffer = CFDataGetBytePtr(rawData)
+        height = CGImageGetHeight(imageRef)
+        width = CGImageGetWidth(imageRef)
+        
+        def coordinatesToOffset(x, y):
+            # Each y increase is a full row, which adds `width` pixels to the offset.
+            # Each pixel has 4 components.
+            return (y * width + x) * 4
+        
+        offset = coordinatesToOffset(round(selectionPoint.x), round(selectionPoint.y))
+        return {"r": buffer[offset+1], "g": buffer[offset+2], "b": buffer[offset+3], "a": buffer[offset]}
+
+    def annotationsForCategory_(self, categoryName):
+        category = self.categoryForName_(categoryName)
+        return self.currentDocument().annotationsForCategory_(category)
+
