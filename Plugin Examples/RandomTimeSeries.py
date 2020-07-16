b94eb02ac305d66caba9093f69aae6b4f52e7178
Johannes Maas <johannes.maas1@rwth-aachen.de>
2020-07-16 19:47:19 +0200

    Add random time series plugin

---
+++ Plugin Examples/RandomTimeSeries.py
@@ -0,0 +1,16 @@
+from Foundation import *
+import objc
+import random
+PluginClass = objc.lookUpClass('AnnotationDataAnalysisPlugin')
+class RandomTimeSeries(PluginClass):
+    def setup(self):
+         # Sets the name of the plugin in the menu
+        self.setDisplayName_("Random Time Series")
+    def performAnalysis(self):
+        series = self.newTimeSeries()
+        (value, timescale, _, _) = self.currentDocument().duration()
+        duration = value/timescale
+        self.log_(duration)
+        for second in range(0, duration + 1):
+            self.log_(second)
+            series.addValue_atSeconds_(random.randrange(1, 100), second)
